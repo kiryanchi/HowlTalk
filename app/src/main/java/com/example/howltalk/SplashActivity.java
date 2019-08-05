c07c55354ef014006c28e087b4c9eb3ac7d5e5fc
kiryanchi <kiryanchi@naver.com>
2019-08-05 10:40:05 +0900

    FireBase 연동 완료

--- app/src/main/java/com/example/howltalk/SplashActivity.java
+++ app/src/main/java/com/example/howltalk/SplashActivity.java
@@ -3,12 +3,17 @@ package com.example.howltalk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        linearLayout = (linearLayout)findViewById(R.id.splashacivity_linearlayout);
     }
 }

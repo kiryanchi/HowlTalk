c16f04e7de35871e621882dbe8f82c1a4e6cdb78
kiryanchi <kiryanchi@naver.com>
2019-08-05 15:47:11 +0900

    회원가입시 image 를 firebase 연동 (Test)

--- app/src/main/java/com/example/howltalk/SignupActivity.java
+++ app/src/main/java/com/example/howltalk/SignupActivity.java
@@ -1,14 +1,20 @@
 package com.example.howltalk;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.example.howltalk.model.UserModel;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -17,16 +23,20 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
 
 public class SignupActivity extends AppCompatActivity {
 
+    private static final int PICK_FROM_ALBUM = 10;
     private EditText email;
     private EditText name;
     private EditText password;
-
     private Button signup;
-
     private FirebaseRemoteConfig mFirebaseRemoteConfig;
+    private ImageView profile;
+    private Uri imageUri;
     String splash_background;
 
     @Override
@@ -40,6 +50,15 @@ public class SignupActivity extends AppCompatActivity {
             getWindow().setStatusBarColor(Color.parseColor(splash_background));
         }
 
+        profile = (ImageView) findViewById(R.id.signupActivity_imageview_profile);
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                startActivityForResult(intent,PICK_FROM_ALBUM);
+            }
+        });
         email = (EditText) findViewById(R.id.signupActivity_edittext_email);
         name = (EditText) findViewById(R.id.signupActivity_edittext_name);
         password = (EditText) findViewById(R.id.signupActivity_edittext_pw);
@@ -60,15 +79,38 @@ public class SignupActivity extends AppCompatActivity {
                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
-                                UserModel userModel = new UserModel();
-                                userModel.userName = name.getText().toString();
+                                final String uid = task.getResult().getUser().getUid();
 
-                                String uid = task.getResult().getUser().getUid();
-                                FirebaseDatabase.getInstance().getReference().child("users").child(uid).setValue(userModel);
+                                StorageTask<UploadTask.TaskSnapshot> userImages = FirebaseStorage.getInstance().getReference().child("userImages").child(uid).putFile(imageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                                        @SuppressWarnings("VisibleForTests")
+                                        Task<Uri> imageUrl = task.getResult().getStorage().getDownloadUrl();
+                                        while(!imageUrl.isComplete());
+
+                                        UserModel userModel = new UserModel();
+
+                                        userModel.userName = name.getText().toString();
+                                        userModel.profileImageUrl = imageUrl.getResult().toString();
+
+
+
+                                        FirebaseDatabase.getInstance().getReference().child("users").child(uid).setValue(userModel);
+                                    }
+                                });
+
                             }
                         });
 
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == PICK_FROM_ALBUM && resultCode == RESULT_OK){
+            profile.setImageURI(data.getData()); // 가운데 뷰를 바
+            imageUri = data.getData(); // 이미지 경로 원본
+        }
     }
 }
